658781dc4645bdcb66cb6c503bbd4a31772e7b04
Kohei Watanabe <kou029w@gmail.com>
2021-02-18 15:58:08 +0900

    feat: ブック削除時トピックは削除しない - ブックからの参照の有無に応じて振る舞いが異なり一貫性に欠け複雑なため

--- server/utils/book/destroyBook.ts
+++ server/utils/book/destroyBook.ts
@@ -1,24 +1,14 @@
 import { Book } from "@prisma/client";
 import prisma from "$server/utils/prisma";
-import destroyTopic from "$server/utils/topic/destroyTopic";
 import cleanupSections from "./cleanupSections";
 
 async function destroyBook(id: Book["id"]) {
-  const topicIds = (
-    await prisma.topicSection.findMany({
-      where: { section: { bookId: id } },
-      select: { topicId: true },
-    })
-  ).map(({ topicId }) => topicId);
-
   try {
     await prisma.$transaction([
       ...cleanupSections(id),
       prisma.ltiResourceLink.deleteMany({ where: { bookId: id } }),
       prisma.book.deleteMany({ where: { id } }),
     ]);
-
-    await Promise.all(topicIds.map(destroyTopic));
   } catch {
     return;
   }
