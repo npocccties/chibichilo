c3c6b984f508258af2b9ad6f4a166a46d4f96e24
FUJII Hiroshi <fujii@acutus.co.jp>
2023-04-14 13:34:19 +0900

    feat: ブックをリリースするAPI追加(DBアクセスは未実装)

--- server/config/routes/book.ts
+++ server/config/routes/book.ts
@@ -7,6 +7,7 @@ import * as authorsService from "$server/services/book/authors";
 import * as showPublicService from "$server/services/book/public";
 import * as showZoomService from "$server/services/book/zoom";
 import * as importService from "$server/services/bookImport";
+import * as releaseService from "$server/services/book/release";
 
 const basePath = "/book";
 const pathWithParams = `${basePath}/:book_id`;
@@ -84,6 +85,7 @@ export async function bookZoom(fastify: FastifyInstance) {
     handler(method)
   );
 }
+
 export async function bookImport(fastify: FastifyInstance) {
   const path = `${pathWithParams}/import`;
   const { importSchema, importHooks, importBook } = importService;
@@ -93,4 +95,15 @@ export async function bookImport(fastify: FastifyInstance) {
     Params: service.Params;
     Body: importService.Params;
   }>(path, { schema: importSchema, ...hooks.post }, handler(importBook));
+}
+
+export async function bookRelease(fastify: FastifyInstance) {
+  const path = `${pathWithParams}/release`;
+  const { method, update } = releaseService;
+  const hooks = makeHooks(fastify, releaseService.hooks);
+
+  fastify.put<{
+    Params: releaseService.Params;
+    Body: releaseService.Props;
+  }>(path, { schema: method.put, ...hooks.put }, handler(update));
 }
