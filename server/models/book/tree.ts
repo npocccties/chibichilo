8360be6fc84305a3fbbb806ff84d941d1da08644
FUJII Hiroshi <fujii@acutus.co.jp>
2023-04-27 16:30:12 +0900

    feat: ブックのツリー情報を取得するAPI追加(DBアクセスは未実装)

---
+++ server/models/book/tree.ts
@@ -0,0 +1,63 @@
+import type { Book, Node } from "@prisma/client";
+import { AuthorSchema } from "../author";
+import type { ReleaseSchema } from "./release";
+import { releaseSchema } from "./release";
+
+export type TreeNodeAuthorsSchema = Pick<
+  AuthorSchema,
+  "id" | "name" | "email" | "roleName"
+>;
+
+export const TreeNodeAuthorsSchema = {
+  type: "object",
+  required: ["id", "name", "email", "roleName"],
+  properties: {
+    id: AuthorSchema.properties.id,
+    name: AuthorSchema.properties.name,
+    email: AuthorSchema.properties.email,
+    roleName: AuthorSchema.properties.roleName,
+  },
+  additionalProperties: false,
+} as const;
+
+export type TreeNodeSchema = Pick<
+  Node,
+  "id" | "parentId"
+> & Pick<
+  Book,
+  "name" | "description" | "createdAt" | "updatedAt"
+> & {
+  authors?: TreeNodeAuthorsSchema;
+  release?: ReleaseSchema;
+};
+
+export const TreeNodeSchema = {
+  type: "object",
+  required: ["id"],
+  properties: {
+    id: { type: "integer" },
+    parentId: { type: "integer", nullable: true },
+    name: { type: "string", nullable: true },
+    description: { type: "string", nullable: true },
+    createdAt: { type: "string", format: "date-time", nullable: true },
+    updatedAt: { type: "string", format: "date-time", nullable: true },
+    authors: { type: "array", items: TreeNodeAuthorsSchema, nullable: true },
+    release: { ...releaseSchema, nullable: true },
+  },
+  additionalProperties: false,
+} as const;
+
+export type TreeResultSchema = {
+  rootId: Node["rootId"];
+  nodes: TreeNodeSchema[];
+};
+
+export const TreeResultSchema = {
+  type: "object",
+  required: ["rootId", "nodes"],
+  properties: {
+    rootId: { type: "integer" },
+    nodes: { type: "array", items: TreeNodeSchema },
+  },
+  additionalProperties: false,
+} as const;
---
+++ server/services/book/tree/index.ts
@@ -0,0 +1,15 @@
+import type { BookParams } from "$server/validators/bookParams";
+import { showSchema, showHooks, show } from "./show";
+
+
+export type Params = BookParams;
+
+export const method = {
+  get: showSchema,
+};
+
+export const hooks = {
+  get: showHooks,
+};
+
+export { show };
