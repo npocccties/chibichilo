f48fc75bb495b077bccfb865d2ef2a77f5c72096
Kohei Watanabe <nebel@fogtype.com>
2023-05-18 16:22:21 +0900

    fix: sectionIdフィールドが含まれフォークに失敗する問題を修正

--- server/utils/book/bookToBookSchema.ts
+++ server/utils/book/bookToBookSchema.ts
@@ -1,12 +1,10 @@
-import type { Prisma, Section, TopicSection } from "@prisma/client";
+import type { Prisma } from "@prisma/client";
 import type { BookSchema } from "$server/models/book";
 import type { SectionSchema } from "$server/models/book/section";
-import type { TopicSchema } from "$server/models/topic";
 import {
   authorArg,
   authorToAuthorSchema,
 } from "$server/utils/author/authorToAuthorSchema";
-import type { TopicWithResource } from "$server/utils/topic/topicToTopicSchema";
 import {
   topicsWithResourcesArg,
   topicToTopicSchema,
@@ -58,13 +56,9 @@ export const getBookIncludingArg = (userId: number) => {
   } as const;
 };
 
-type TopicSectionWithTopic = TopicSection & {
-  topic: TopicWithResource;
-};
-
-type SectionWithTopics = Section & {
-  topicSections: TopicSectionWithTopic[];
-};
+type SectionWithTopics = Prisma.SectionGetPayload<
+  ReturnType<typeof getBookIncludingArg>["include"]["sections"]
+>;
 
 export type BookWithTopics = Prisma.BookGetPayload<
   typeof bookIncludingTopicsArg
@@ -83,16 +77,7 @@ function sectionToSectionSchema(section: SectionWithTopics): SectionSchema {
   return {
     ...section,
     topics: section.topicSections.map((topicSection) =>
-      topicSectionToTopicSchema(topicSection)
+      topicToTopicSchema(topicSection.topic)
     ),
-  };
-}
-
-function topicSectionToTopicSchema(
-  topicSection: TopicSectionWithTopic
-): TopicSchema {
-  return {
-    ...topicSection,
-    ...topicToTopicSchema(topicSection.topic),
   };
 }
