f22ff677b94d78b7d7fab3e2b9b23c2ecc36dfdb
FUJII Hiroshi <fujii@acutus.co.jp>
2023-05-26 17:31:32 +0900

    バージョンツリーダイアログの追加

---
+++ components/organisms/VersionTreeDialog.tsx
@@ -0,0 +1,58 @@
+import Placeholder from "$templates/Placeholder";
+import useBookTree from "$utils/useBookTree";
+import BookTreeDiagram from "$templates/BookTreeDiagram";
+import type { TreeNodeType } from "$templates/BookTreeDiagram";
+import type { BookSchema } from "$server/models/book";
+import React, { useState } from "react";
+import type { TreeNodeSchema } from "$server/models/book/tree";
+import BookTreeDialog from "$organisms/BookTreeDialog";
+import Dialog from "@mui/material/Dialog";
+import IconButton from "$atoms/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+
+export type VersionTreeDialogProps = {
+  book: BookSchema;
+  bookId: BookSchema["id"];
+  open: boolean;
+  onClose: React.MouseEventHandler;
+};
+
+export function VersionTreeDialog(props: VersionTreeDialogProps) {
+  const { book, bookId, open, onClose } = props;
+  const { tree, error } = useBookTree(bookId);
+  const [nodeType, setNodeType] = useState<TreeNodeType | undefined>();
+  const [node, setNode] = useState<TreeNodeSchema | undefined>();
+
+  if (error || !tree) return <Placeholder />;
+
+  function onNodeClick(
+    nodeType: TreeNodeType,
+    node: TreeNodeSchema | undefined
+  ) {
+    setNodeType(nodeType);
+    setNode(node);
+  }
+
+  const handleClose = () => {
+    setNode(undefined);
+  };
+
+  return (
+    <Dialog fullScreen open={open} onClose={onClose}>
+      <IconButton
+        tooltipProps={{ title: "閉じる" }}
+        onClick={onClose}
+        size="large"
+      >
+        <CloseIcon />
+      </IconButton>
+      <BookTreeDiagram book={book} tree={tree} onNodeClick={onNodeClick} />;
+      <BookTreeDialog
+        nodeType={nodeType}
+        node={node}
+        open={node != null}
+        onClose={handleClose}
+      />
+    </Dialog>
+  );
+}
