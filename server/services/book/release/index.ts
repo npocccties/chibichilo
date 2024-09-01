1a3a3b80d90741c09f30dc92fd9109a811091761
FUJII Hiroshi <fujii@acutus.co.jp>
2024-09-01 20:17:04 +0900

    POSTリリースAPI新設

--- server/services/book/release/index.ts
+++ server/services/book/release/index.ts
@@ -1,16 +1,19 @@
 import type { ReleaseProps } from "$server/models/book/release";
 import type { BookParams } from "$server/validators/bookParams";
+import { createSchema, createHooks, create } from "./create";
 import { updateSchema, updateHooks, update } from "./update";
 
 export type Params = BookParams;
 export type Props = ReleaseProps;
 
 export const method = {
+  post: createSchema,
   put: updateSchema,
 };
 
 export const hooks = {
+  post: createHooks,
   put: updateHooks,
 };
 
-export { update };
+export { create, update };
