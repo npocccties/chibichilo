7da09883a26a05e53ccae2822772550b491daf23
FUJII Hiroshi <fujii@acutus.co.jp>
2024-11-28 21:57:08 +0900

    シェア廃止 serverの変更

--- server/utils/displayableBook.ts
+++ server/utils/displayableBook.ts
@@ -5,7 +5,7 @@ import type { IsContentEditable } from "$server/models/content";
 import contentBy from "./contentBy";
 
 export function isDisplayableBook(
-  book: Pick<BookSchema, "id" | "shared" | "authors">,
+  book: Pick<BookSchema, "id" | "shared" | "authors" | "release">,
   isContentEditable: IsContentEditable | undefined,
   ltiResourceLink:
     | Pick<LtiResourceLinkSchema, "bookId" | "creatorId">
@@ -13,11 +13,11 @@ export function isDisplayableBook(
   publicBook?: PublicBookSchema
 ) {
   const linked = book.id === ltiResourceLink?.bookId;
-  return book.shared || linked || isContentEditable?.(book) || publicBook;
+  return book.release?.shared || linked || isContentEditable?.(book) || publicBook;
 }
 
 export function getDisplayableBook<
-  Book extends Pick<BookSchema, "id" | "shared" | "authors" | "sections">,
+  Book extends Pick<BookSchema, "id" | "shared" | "authors" | "sections" | "release">,
 >(
   book: Book | undefined,
   isContentEditable: IsContentEditable | undefined,
@@ -31,7 +31,6 @@ export function getDisplayableBook<
   const sections = book.sections.flatMap((section) => {
     const topics = section.topics.filter(
       (topic) =>
-        topic.shared ||
         contentBy(topic, { id: ltiResourceLink?.creatorId }) ||
         (publicBook && contentBy(topic, { id: publicBook.userId })) ||
         isContentEditable?.(topic)
