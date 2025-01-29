53b61dd0f257a722f30a943af88a0a643649f54b
FUJII Hiroshi <fujii@acutus.co.jp>
2025-01-29 20:50:57 +0900

    UI: ブックのリリースでトピック選択を廃止

--- components/templates/ReleaseEdit.tsx
+++ components/templates/ReleaseEdit.tsx
@@ -3,13 +3,6 @@ import Container from "$atoms/Container";
 import type { BookSchema } from "$server/models/book";
 import ReleaseForm from "$organisms/ReleaseForm";
 import type { ReleaseProps } from "$server/models/book/release";
-import TreeView from "@mui/lab/TreeView";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import SectionsTree from "$molecules/SectionsTree";
-import Card from "@mui/material/Card";
-import useCardStyles from "$styles/card";
-import { useEffect, useState } from "react";
 
 export type Props = {
   book: BookSchema;
@@ -18,33 +11,8 @@ export type Props = {
 
 function ReleaseEdit(props: Props) {
   const { book, onSubmit } = props;
-  const cardClasses = useCardStyles();
-  const [selectedNodeIds, select] = useState<Set<string>>(new Set());
-  useEffect(() => {
-    const nodeIds: string[] = [];
-    book.sections.map((section) => {
-      section.topics.map((topic, topicIndex) => {
-        const nodeId = `${book.id}-${section.id}-${topic.id}:${topicIndex}`;
-        nodeIds.push(nodeId);
-      });
-    });
-    select(() => new Set(nodeIds));
-  }, [book, select]);
-  const handleTreeChange = (nodeId: string) => {
-    select((nodeIds) =>
-      nodeIds.delete(nodeId) ? new Set(nodeIds) : new Set(nodeIds.add(nodeId))
-    );
-  };
   const handleSubmit = (props: ReleaseProps) => {
-    const topics: number[] = [];
-    selectedNodeIds.forEach((nodeId) => {
-      const [_bookId, _sectionId, topicId] = nodeId
-        .replace(/:[^:]*$/, "")
-        .split("-")
-        .map((id) => Number(id));
-      topics.push(topicId);
-    });
-    onSubmit({ ...props, topics });
+    onSubmit({ ...props });
   };
   return (
     <Container
@@ -58,19 +26,6 @@ function ReleaseEdit(props: Props) {
     >
       <Typography variant="h4">{book.name}</Typography>
       <ReleaseForm release={book.release ?? {}} onSubmit={handleSubmit} />
-      <Card classes={cardClasses}>
-        <TreeView
-          defaultCollapseIcon={<ExpandMoreIcon />}
-          defaultExpandIcon={<ChevronRightIcon />}
-        >
-          <SectionsTree
-            bookId={book.id}
-            sections={book.sections}
-            onTreeChange={handleTreeChange}
-            selectedIndexes={selectedNodeIds}
-          />
-        </TreeView>
-      </Card>
     </Container>
   );
 }
