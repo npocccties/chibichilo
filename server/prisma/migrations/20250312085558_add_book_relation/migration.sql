226776b96100d387d21a33c0e8b8d33730d5223a
Yu Tsuda <yu.tsuda@gmail.com>
2025-03-12 18:21:04 +0900

    fix: 学習活動にブックの関連を入れるマイグレーションを統合する

--- server/prisma/migrations/20250312085558_add_book_relation/migration.sql
+++ server/prisma/migrations/20250312085558_add_book_relation/migration.sql
@@ -2,17 +2,22 @@
   Warnings:
 
   - A unique constraint covering the columns `[book_id,topic_id,learner_id,lti_consumer_id,lti_context_id]` on the table `activities` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `book_id` to the `activities` table without a default value. This is not possible if the table is not empty.
 
 */
 -- DropIndex
 DROP INDEX "activities_topic_id_learner_id_lti_consumer_id_lti_context__key";
 
 -- AlterTable
-ALTER TABLE "activities" ADD COLUMN     "book_id" INTEGER NOT NULL;
+ALTER TABLE "Bookmark" ADD COLUMN     "book_id" INTEGER;
+
+-- AlterTable
+ALTER TABLE "activities" ADD COLUMN     "book_id" INTEGER;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "activities_book_id_topic_id_learner_id_lti_consumer_id_lti__key" ON "activities"("book_id", "topic_id", "learner_id", "lti_consumer_id", "lti_context_id");
 
 -- AddForeignKey
-ALTER TABLE "activities" ADD CONSTRAINT "activities_book_id_fkey" FOREIGN KEY ("book_id") REFERENCES "books"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "activities" ADD CONSTRAINT "activities_book_id_fkey" FOREIGN KEY ("book_id") REFERENCES "books"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Bookmark" ADD CONSTRAINT "Bookmark_book_id_fkey" FOREIGN KEY ("book_id") REFERENCES "books"("id") ON DELETE SET NULL ON UPDATE CASCADE;
