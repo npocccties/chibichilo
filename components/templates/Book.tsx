e979b4244f7f11f14f73d024a840f9654e37cf9a
ties-makimura <makimura@cccties.org>
2025-06-03 17:20:31 +0900

    feat: スクロール時のヘッダー非表示
    #1105

--- components/templates/Book.tsx
+++ components/templates/Book.tsx
@@ -43,6 +43,9 @@ const useStyles = makeStyles((theme) => ({
       flexShrink: 0,
     },
   },
+  headerHidden: {
+    visibility: 'hidden',
+  },
   title: {
     fontSize: "1.75rem",
     overflow: "hidden",
@@ -176,7 +179,9 @@ export default function Book(props: Props) {
       disableGutters={!matches}
     >
       <ActionHeader sx={{ pb: 0 }} considerAppBar={considerAppBar}>
-        <header className={classes.header}>
+        <header className={clsx(classes.header, {
+          [classes.headerHidden]: trigger
+        })}>
           <Typography
             className={clsx(classes.title, { [classes.mobile]: !matches })}
             variant="h4"
