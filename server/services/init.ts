f67ce5d4306eacd615fdce649115d4301000b972
FUJII Hiroshi <fujii@acutus.co.jp>
2025-10-06 21:35:20 +0900

    feat: LTIリンク初期化とアクセス制御の改善

--- server/services/init.ts
+++ server/services/init.ts
@@ -1,7 +1,10 @@
 import type { FastifyRequest } from "fastify";
 import type { LtiResourceLinkSchema } from "$server/models/ltiResourceLink";
 import { FRONTEND_ORIGIN, FRONTEND_PATH } from "$server/utils/env";
-import { upsertUser } from "$server/utils/user";
+import {
+  findUserByEmailAndLtiConsumerId,
+  upsertUser,
+} from "$server/utils/user";
 import {
   findLtiResourceLink,
   upsertLtiResourceLink,
@@ -9,8 +12,31 @@ import {
 import { isInstructor } from "$server/utils/ltiv1p3/roles";
 import { getSystemSettings } from "$server/utils/systemSettings";
 import getValidUrl from "$server/utils/getValidUrl";
+import { getInstructors } from "$server/utils/ltiv1p3/services";
+import type { FastifySessionObject } from "@fastify/session";
+import { isAdministrator } from "$server/utils/ltiv1p3/roles";
 
 const frontendUrl = `${FRONTEND_ORIGIN}${FRONTEND_PATH}`;
+
+async function getCreatorByNRPS(session: FastifySessionObject) {
+  const instructors = await getInstructors(session);
+  let creatorId: number | undefined;
+  if (instructors?.members) {
+    const instructor = instructors.members.find((member) => {
+      return !isAdministrator(member.roles);
+    });
+    if (instructor) {
+      const user = await findUserByEmailAndLtiConsumerId(
+        instructor.email || "",
+        session.oauthClient.id
+      );
+      if (user) {
+        creatorId = user.id;
+      }
+    }
+  }
+  return creatorId;
+}
 
 /** 起動時の初期化プロセス */
 async function init({ session }: FastifyRequest) {
@@ -51,14 +77,17 @@ async function init({ session }: FastifyRequest) {
       ? ltiTargetLink.href
       : undefined;
   if (
-    isInstructor(session.ltiRoles) &&
     session.ltiMessageType === "LtiResourceLinkRequest" &&
     session.ltiResourceLinkRequest?.id &&
     Boolean(ltiTargetLinkUri)
   ) {
+    let creatorId: number | undefined;
+    if (!ltiResourceLink?.creatorId && !isInstructor(session.ltiRoles)) {
+      creatorId = await getCreatorByNRPS(session);
+    }
     ltiResourceLink = {
       bookId: Number(bookId),
-      creatorId: ltiResourceLink?.creatorId ?? user.id, // 直接user.idを使用
+      creatorId: ltiResourceLink?.creatorId ?? creatorId ?? user.id, // 直接user.idを使用
       consumerId: session.oauthClient.id,
       contextId: session.ltiContext.id,
       id: session.ltiResourceLinkRequest.id,
@@ -71,7 +100,7 @@ async function init({ session }: FastifyRequest) {
     };
   }
 
-  if (ltiResourceLink) {
+  if (ltiResourceLink && isInstructor(session.ltiRoles)) {
     await upsertLtiResourceLink({
       ...ltiResourceLink,
       title: session.ltiResourceLinkRequest?.title ?? ltiResourceLink.title,
