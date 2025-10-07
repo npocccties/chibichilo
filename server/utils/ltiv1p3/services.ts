b4a281770347042cb9ad24e424e9bf46822df37b
FUJII Hiroshi <fujii@acutus.co.jp>
2025-10-07 09:24:23 +0900

    getInstructors の retry 機能を修正

--- server/utils/ltiv1p3/services.ts
+++ server/utils/ltiv1p3/services.ts
@@ -43,7 +43,7 @@ export async function getMemberships(
     });
 
     if (retry) {
-      return await getMemberships(client, contextMembershipsUrl, false);
+      return await getMemberships(client, contextMembershipsUrl, false, query);
     }
   }
 
