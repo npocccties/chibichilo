d2ce21234bdb54e88fb4f4f53afbd74111bebccf
Youhei Nozaki <youhey.hey0505@gmail.com>
2026-09-24 22:50:51 +0900

    chore: update TypeScript configuration and Storybook setup for improved module resolution and include additional Storybook files

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -8,9 +8,7 @@ import {
 import CssBaseline from "@mui/material/CssBaseline";
 import { ConfirmProvider } from "material-ui-confirm";
 import theme from "../theme";
-// NOTE: For VideoJs components.
-import "video.js/dist/video-js.css";
-import "videojs-seek-buttons/dist/videojs-seek-buttons.css";
+import "@videojs/react/video/skin.css";
 
 function ThemeProvider({ children }: { children: ReactNode }) {
   return (
